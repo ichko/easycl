b153abd0dc960d2a66382fe2584abc778d18725f
Iliya Zhechev <i4ko95@gmail.com>
2017-05-14 13:31:03 +0300

    EasySDL

--- easycl/main.cpp
+++ easycl/main.cpp
@@ -1,54 +1,35 @@
 #include "easycl.hpp"
-#include "sdl.hpp"
-#include <time.h>
-#include <chrono>
-#include <ctime>
+#include "easysdl.hpp"
 
-clock_t tStart = clock();
-float get_time() {
-    return (clock() - tStart) / 2000.0;
-}
 
 int main(int argc, char** argv) {
-    const size_t width = 600;
-    const size_t height = 600;
-    const size_t pixels_cnt = width * height;
-    int* screen_buffer = new int[width * height];
+    auto easysdl = EasySDL()
+        .init()
+        .set_window(500, 500);
+        //.set_window_full_screen();
 
-    auto sdl_context = SDLContext().init(width, height);
-    float time = get_time();
-
-    auto cl = EasyCL()
+    auto easycl = EasyCL()
         .load_device(0, 1)
         .load_src("shader.cl")
         .load_kernel("start")
-        .set_arg(0, screen_buffer, pixels_cnt)
-        .set_arg(1, &width)
-        .set_arg(2, &height)
-        .set_arg(3, &time)
-        .run(pixels_cnt)
-        .read_buffer(0, screen_buffer, pixels_cnt);
+        .set_arg(0, easysdl.screen_buffer, easysdl.screen_buffer_size)
+        .set_arg(1, &easysdl.width)
+        .set_arg(2, &easysdl.height)
+        .set_arg(3, &easysdl.timer)
+        .run(easysdl.screen_buffer_size)
+        .read_buffer(0, easysdl.screen_buffer, easysdl.screen_buffer_size);
 
-    while (true) {
-        time = get_time();
-        cl
-            .update_arg(3, &time)
-            .run(pixels_cnt)
-            .read_buffer(0, screen_buffer, pixels_cnt);
-        sdl_context.render(screen_buffer);
+    while (!easysdl.key_down(SDLK_ESCAPE)) {
+        easycl
+            .update_arg(3, &easysdl.timer)
+            .run(easysdl.screen_buffer_size)
+            .read_buffer(0, easysdl.screen_buffer, easysdl.screen_buffer_size);
+
+        easysdl
+            .tick()
+            .render();
     }
-    sdl_context.destroy();
-    delete[] screen_buffer;
-
-    //for (size_t i = 0; i < width; i++) {
-    //    for (size_t j = 0; j < height; j++) {
-    //        std::cout << "[" <<
-    //            screen_buffer[i * j].x << "," <<
-    //            screen_buffer[i * j].y << "," <<
-    //            screen_buffer[i * j].z << "]";
-    //    }
-    //    std::cout << std::endl;
-    //}
+    easysdl.destroy();
 
     return 0;
 }
