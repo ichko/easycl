bc6f998e8635c4ecb9331bdc09707d90fe3ceb47
Iliya Zhechev <i4ko95@gmail.com>
2017-06-23 22:54:56 +0300

    Removing CL and wip on build.sh

--- easycl/build.sh
+++ easycl/build.sh
@@ -4,5 +4,5 @@ include_dir=/usr/local/cuda-8.0/include/
 rm -rf ./$build_dir
 mkdir ./$build_dir
 
-g++ -std=c++11 main.cpp -o ./$build_dir -I$include_dir -lOpenCL -Wall 
+g++ -std=c++1y main.cpp -o ./$build_dir -I$include_dir -lOpenCL -Wall 
 
