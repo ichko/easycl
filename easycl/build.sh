20b033a8fc129a3fda9319b15bec6e70802230f7
Iliya Zhechev <i4ko95@gmail.com>
2017-06-23 22:39:13 +0300

    Removing CL and updating build.sh

--- easycl/build.sh
+++ easycl/build.sh
@@ -1,4 +1,8 @@
 build_dir=dist
+include_dir=/usr/local/cuda-8.0/include/
+
 rm -rf ./$build_dir
 mkdir ./$build_dir
-g++ *.cpp -o ./$build_dir
+
+g++ -std=c++11 main.cpp -o ./$build_dir -I$include_dir -lOpenCL -Wall 
+
